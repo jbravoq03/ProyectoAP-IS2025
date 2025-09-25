071d82293e853c5277ff0211022e7233953a68f5
jbravoq03 <160434599+jbravoq03@users.noreply.github.com>
2025-09-25 15:12:38 -0600

    Modulo administradores - CRUD y Pruebas
    CRUD de roles - Probado y CRUD de departamentos/carreras - Probado...

--- FlaskAPI/API/api_modulo_administradores.py
+++ FlaskAPI/API/api_modulo_administradores.py
@@ -1,29 +1,213 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
+from Controladores_Tablas.controlador_roles import *
+from Controladores_Tablas.controlador_dptoCarrera import *
 
+# Se define el blueprint
 administradores_bp = Blueprint('administradores',
                             __name__,
                             url_prefix='/administradores')
 
+#Comprobacion de estado de los endpoints
 @administradores_bp.route('/status', methods=['GET'])
 def statusAPIAdministradores():
     return "¡API de administradores funcionando!"
 
-@administradores_bp.route('/create', methods=['POST'])
-def create_administradores():
+#-------------------------------------
+# Tabla de Roles
 
-    return "Crear"
+#Cuerpo del JSON para los POST:
+"""
+{
+    "idRol": "",
+    "nombre": ""
+}
+"""
+#Endpoint para crear roles
+@administradores_bp.route('/rol/create', methods=['POST'])
+def create_roles():
+    # Lectura del JSON
+    entrada = request.get_json()
+    nombre = entrada["nombre"]
 
-@administradores_bp.route('/read', methods=['GET'])
-def read_administradores():
+    # Creacion del rol en la BD
+    respuesta = createRoles(nombre)
 
-    return "leer"
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data,
+        "count": getattr(respuesta, "count", None)
+    })
 
-@administradores_bp.route('/readu', methods=['POST'])
-def readUnique_administradores():
+    # Retorno de la respuesta
+    return resp
 
-    return "leer uno"
+#Endpoint que retorna todos los roles
+@administradores_bp.route('/rol/read', methods=['GET'])
+def read_roles():
+    # Lectura de los roles en la BD
+    respuesta = readRoles()
 
-@administradores_bp.route('/delete', methods=['POST'])
-def delete_administradores():
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data
+    })
 
-    return "borrar"
+    # Retorno de la respuesta
+    return resp
+
+#Endpoint que retorna un rol
+@administradores_bp.route('/rol/readu', methods=['POST'])
+def readUnique_roles():
+    # Lectura del JSON
+    entrada = request.get_json()
+    idRol = entrada["idRol"]
+
+    # Lectura de un rol en la BD
+    respuesta = readOneRoles(idRol)
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data
+    })
+
+    # Retorno de la respuesta
+    return resp
+
+#Endpoint para modificar roles
+@administradores_bp.route('/rol/update', methods=['POST'])
+def update_roles():
+    #Lectura del JSON
+    entrada = request.get_json()
+    idRol = entrada["idRol"]
+    nombre = entrada["nombre"]
+
+    #Se modifica el rol en la BD
+    respuesta = updateRoles(idRol, nombre)
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data,
+        "count": getattr(respuesta, "count", None)
+    })
+
+    # Retorno de la respuesta
+    return resp
+
+#Endpoint que elimina un rol
+@administradores_bp.route('/rol/delete', methods=['POST'])
+def delete_roles():
+    #Lectura del JSON
+    entrada = request.get_json()
+    idRol = entrada["idRol"]
+
+    #Se elimina el rol en la BD
+    respuesta = deleteRoles(idRol)
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data,
+        "count": getattr(respuesta, "count", None)
+    })
+
+    # Retorno de la respuesta
+    return resp
+
+#-------------------------------------
+# Tabla de Departamentos/Carreras
+
+#Cuerpo del JSON para los POST:
+"""
+{
+    "idDc": "",
+    "nombre": ""
+}
+"""
+#Endpoint para crear departamentos/carreras
+@administradores_bp.route('/dptocar/create', methods=['POST'])
+def create_dptocar():
+    # Lectura del JSON
+    entrada = request.get_json()
+    nombre = entrada["nombre"]
+
+    # Creacion del departamento/carrera en la BD
+    respuesta = createDptoCarrera(nombre)
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data,
+        "count": getattr(respuesta, "count", None)
+    })
+
+    # Retorno de la respuesta
+    return resp
+
+#Endpoint que retorna todos los departamentos/carreras
+@administradores_bp.route('/dptocar/read', methods=['GET'])
+def read_dptocar():
+    # Lectura de los departamentos/carreras en la BD
+    respuesta = readDptoCarrera()
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data
+    })
+
+    # Retorno de la respuesta
+    return resp
+
+#Endpoint que retorna un departamento/carrera
+@administradores_bp.route('/dptocar/readu', methods=['POST'])
+def readUnique_dptocar():
+    # Lectura del JSON
+    entrada = request.get_json()
+    idDc = entrada["idDc"]
+
+    # Lectura de un departamento/carrera en la BD
+    respuesta = readOneDptoCarrera(idDc)
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data
+    })
+
+    # Retorno de la respuesta
+    return resp
+
+#Endpoint para modificar departamento/carrera
+@administradores_bp.route('/dptocar/update', methods=['POST'])
+def update_dptocar():
+    #Lectura del JSON
+    entrada = request.get_json()
+    idDc = entrada["idDc"]
+    nombre = entrada["nombre"]
+
+    #Se modifica el departamento/carrera en la BD
+    respuesta = updateDptoCarrera(idDc, nombre)
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data,
+        "count": getattr(respuesta, "count", None)
+    })
+
+    # Retorno de la respuesta
+    return resp
+
+#Endpoint que elimina un departamento/carrera
+@administradores_bp.route('/dptocar/delete', methods=['POST'])
+def delete_dptocar():
+    #Lectura del JSON
+    entrada = request.get_json()
+    idDc = entrada["idDc"]
+
+    #Se elimina el departamento/carrera en la BD
+    respuesta = deleteDptoCarrera(idDc)
+
+    # Se convierte la respuesta en un JSON
+    resp = jsonify({
+        "data": respuesta.data,
+        "count": getattr(respuesta, "count", None)
+    })
+
+    # Retorno de la respuesta
+    return resp
--- FlaskAPI/API/api_modulo_usuarios.py
+++ FlaskAPI/API/api_modulo_usuarios.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
 
 usuarios_bp = Blueprint('usuarios',
                             __name__,
@@ -8,21 +8,65 @@ usuarios_bp = Blueprint('usuarios',
 def statusAPIUsuarios():
     return "¡API de usuarios funcionando!"
 
+"""
+Cuerpo del JSON para los POST:
+{
+    "idUsr": "",
+    "idRol": "",
+    "idDc": "",
+    "idInsti": "",
+    "correoInsti": "",
+    "nombre": "",
+    "telefono": "",
+    "contrasena": ""
+}
+"""
+
+#Endpoint para crear usuarios
 @usuarios_bp.route('/create', methods=['POST'])
 def create_Usuarios():
+    entrada = request.get_json()
+
+    idRol = entrada["idRol"]
+    idDc = entrada["idDc"]
+    idInsti = entrada["idInsti"]
+    correoInsti = entrada["correoInsti"]
+    nombre = entrada["nombre"]
+    telefono = entrada["telefono"]
+    contrasena = entrada["contrasena"]
 
     return "Crear"
 
+#Endpoint que retorna todos los usuarios
 @usuarios_bp.route('/read', methods=['GET'])
 def read_Usuarios():
 
     return "leer"
 
+#Endpoint que retorna solo un usuario
 @usuarios_bp.route('/readu', methods=['POST'])
 def readUnique_Usuarios():
 
     return "leer uno"
 
+#Endpoint para modificar un usuario
+@usuarios_bp.route('/update', methods=['POST'])
+def update_Usuarios():
+
+    entrada = request.get_json()
+
+    idUsr = entrada["idUsr"]
+    idRol = entrada["idRol"]
+    idDc = entrada["idDc"]
+    idInsti = entrada["idInsti"]
+    correoInsti = entrada["correoInsti"]
+    nombre = entrada["nombre"]
+    telefono = entrada["telefono"]
+    contrasena = entrada["contrasena"]
+
+    return "Modificar"
+
+#Enpoint para borrar un usuario
 @usuarios_bp.route('/delete', methods=['POST'])
 def delete_Usuarios():
 
