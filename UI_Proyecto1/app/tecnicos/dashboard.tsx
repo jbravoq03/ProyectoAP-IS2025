09cb13aeda4e684fb3cbc3733d1ebd538bf53ee9
jbravoq03 <160434599+jbravoq03@users.noreply.github.com>
2025-09-27 20:23:26 -0600

    Add files via upload

--- UI_Proyecto1/app/tecnicos/dashboard.tsx
+++ UI_Proyecto1/app/tecnicos/dashboard.tsx
@@ -1,124 +1,33 @@
 import { View } from '@/components/Themed';
-import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
-import { Heading } from '@/components/ui/heading';
-import { Text } from '@/components/ui/text';
-import { VStack } from '@/components/ui/vstack';
 import { useRouter } from 'expo-router';
-import { Image, ScrollView, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-
-export default function dashboardTecnicos() {
+export default function dashboardAdmins() {
 
   const router = useRouter();
   const handleLogout = () => {
     // Redirige al inicio de sesion
       router.replace('/');
   };
-  const handleInvent = () => {
-    // Redirige al inicio de sesion
-      router.replace('/laboratorios/inventario');
-  };
 
   return (
-    
     <View style={styles.container}>
-      {/* Inicio Menú principal*/}
-      <View style={styles.horizontalContainer}>
-        <ScrollView 
-          horizontal 
-          showsHorizontalScrollIndicator={false} // oculta la barrita
-          contentContainerStyle={styles.horizontalContainer} // aplica estilos al contenido
-        >
-          <Image
-            source={require('../../assets/images/tec.png')} 
-            style={{ width: 150, height: 40}}
-            resizeMode="contain"
-            alt="Logo"
-          />
-          <Button variant="solid" className="bg-white" size="md" action="primary" >
-            <ButtonText className="text-black">Gestión de Inventario</ButtonText>
-          </Button>
-          <Button variant="solid" className="bg-white" size="md" action="primary">
-            <ButtonText className="text-black">Pantalla de Mantenimiento</ButtonText>
-          </Button>
-          <Button variant="solid" className="bg-white" size="md" action="primary">
-            <ButtonText className="text-black">Panel de Reportes Operativos</ButtonText>
-          </Button>
-          <Button onPress={handleLogout} variant="solid" className="bg-white" size="md" action="primary">
-            <ButtonText className="text-black">Cerrar Sesión</ButtonText>
-          </Button>
-        </ScrollView>
-      </View>
-      {/* Fin Menú principal*/}
 
-      <View style={styles.line} />
-      <Card className="p-5 rounded-lg max-w-[360px] m-3 bg-white border border-black" >
-        <VStack className="mb-6">
-          <Heading size="md" className="mb-4  text-black">
-            Solicitud "x"
-          </Heading>
-          <Text size="sm" className="text-black">
-            Descripcion
-          </Text>
-        </VStack>
-        <Box className="flex-col sm:flex-row">
-          <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-            <ButtonText size="sm">Aceptar</ButtonText>
-          </Button>
-          <Button
-            variant="solid"
-            action="secondary"
-            className="px-4 py-2 border-outline-300 sm:flex-1"
-          >
-            <ButtonText size="sm" className="text-typography-600">
-              Rechazar
-            </ButtonText>
-          </Button>
-        </Box>
-      </Card>
-
-
+      <Button variant="solid" size="md" action="primary"  onPress={handleLogout}>
+        <ButtonText>Cerrar Sesion</ButtonText>
+      </Button>
+      
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center', 
-    backgroundColor: '#ffffffff',
-    padding: 20,
-  },
-  
-  containerCard: {
-    flex: 1,
-    width: '150%',
-    alignItems: 'center', 
-    backgroundColor: '#ffffffff',
-    padding: 50,
-  },
-  card: {
+    alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 40,
-    width: '90%',
-    maxWidth: 300,
-    backgroundColor: '#fff',
-    borderRadius: 12,
-    padding: 20,
-    shadowColor: '#ffffffff',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-   header: {
-    padding: 16,
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#000',
   },
   title: {
     fontSize: 20,
@@ -129,19 +38,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-  line: {
-    marginTop: 10,
-    height: 2,
-    width: '100%',
-    backgroundColor: '#000',
-  },
-  horizontalContainer: {
-  backgroundColor: '#fff',
-  flexDirection: 'row',
-  alignItems: 'center',       // centra verticalmente los elementos
-  justifyContent: 'flex-start', 
-  gap: 8,                     // espacio entre botones e imagen
-  paddingVertical: 15,          // reduce padding vertical
-},  
-
-  });
+});
