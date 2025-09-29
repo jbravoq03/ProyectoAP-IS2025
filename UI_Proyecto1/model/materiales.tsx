e3397cb0eb1bd30badbf6377444d9657bee00050
jbravoq03 <160434599+jbravoq03@users.noreply.github.com>
2025-09-29 17:25:36 -0600

    Actualizacion de model y services
    Por si se hicieron cambios...

--- UI_Proyecto1/model/materiales.tsx
+++ UI_Proyecto1/model/materiales.tsx
@@ -3,30 +3,5 @@ export type Material = {
   idRec: Number;
   cantidad: string;
   medida: string;
-  reorden: boolean;
+  reorden: string;
 };
-
-export const materiales: Material[] = [
-  {
-    idMat: 1,
-    idRec: 1,
-    cantidad: "100",
-    medida: 'Cajas',
-    reorden: false,
-  },
-  {
-    idMat: 2,
-    idRec: 2,
-    cantidad: "12",
-    medida: 'Botellas',
-    reorden: true,
-  },
-  {
-    idMat: 3,
-    idRec: 3,
-    cantidad: "20",
-    medida: 'Disponible',
-    reorden: false,
-  },
-  
-];
