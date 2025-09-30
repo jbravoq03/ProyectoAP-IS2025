d96c5b90bd30accce4ff8b2b8b23f0f194e82abf
jbravoq03 <160434599+jbravoq03@users.noreply.github.com>
2025-09-29 19:46:35 -0600

    Actualizacion models

--- UI_Proyecto1/model/listStorage.tsx
+++ UI_Proyecto1/model/listStorage.tsx
@@ -1,4 +1,6 @@
+import { bitacoraRec } from "./bitacoraRecursos";
 import { Material } from "./materiales";
+import { RecursoFijo } from "./recFijos";
 import { Recurso } from "./recursos";
 import { Responsable } from "./responsables";
 import { Solicitud } from "./solicitudes";
@@ -8,8 +10,20 @@ export let solicitudes: Solicitud[] = [];
 export let recursos: Recurso[] = [];
 export let usuarios: usuario[] = [];
 export let materiales: Material[] = [];
+export let bitacoraRecursos: bitacoraRec[] = [];
+export let recursosFijos: RecursoFijo[] = [];
 
 export let responsable: Responsable | null = null;
+
+export const getRecfijos = () => recursosFijos;
+export const setRecFijos = (nuevaLista: RecursoFijo[]) => {
+  recursosFijos = nuevaLista;
+};
+
+export const getBitRec = () => bitacoraRecursos;
+export const setBitRec = (nuevaLista: bitacoraRec[]) => {
+  bitacoraRecursos = nuevaLista;
+};
 
 export const getSolicitudes = () => solicitudes;
 export const setSolicitudes = (nuevaLista: Solicitud[]) => {
